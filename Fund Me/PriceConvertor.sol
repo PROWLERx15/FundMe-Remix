dbe920d48dab1fdbc6491b60238d18eb1def5e06
Kshitij <162005485+PROWLERx15@users.noreply.github.com>
2024-06-23 03:13:16 +0530

    Update PriceConvertor.sol

--- Fund Me/PriceConvertor.sol
+++ Fund Me/PriceConvertor.sol
@@ -32,8 +32,4 @@ library PriceConvertor
         return ethAmountInUSD;
     }
 
-    function getVersion() public view returns (uint256) 
-    {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
 }
